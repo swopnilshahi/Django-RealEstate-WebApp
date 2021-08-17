954fb62aa6fa2f0069e7a34434750d9e519707f1
swopnilshahi <swopnilshahi8@gmail.com>
2021-08-17 14:53:30 +0545

    logo changed

--- realestate/settings.py
+++ realestate/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'real_estate' ,
-        'USER': 'pks',
-        'PASSWORD': 'abc123!',
-        'HOST':'localhost',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
